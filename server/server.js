35cbd6f1f5cb4bf6a6a48157972ab9f2d4edc715
Adam Plante <toomanyadams@gmail.com>
2018-10-17 23:13:41 -0700

    Add models and base level server.

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/Todo');
+const { User } = require('./models/User');
+
+const app = express();
+const PORT  = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const input = req.body;
+  const todo = new Todo({
+    text: input.text,
+  })
+
+  todo.save().then(
+    (doc) => res.status(201).send(doc),
+    (err) => res.status(400).send(err)
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
+});
