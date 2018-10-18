74772cc06453e6eb5335216505ebfba9cdc86539
Adam Plante <toomanyadams@gmail.com>
2018-10-18 00:10:16 -0700

    Add get all route for Todos.

--- server/server.js
+++ server/server.js
@@ -22,6 +22,13 @@ app.post('/todos', (req, res) => {
   );
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then(
+    (todos => res.send({ todos })),
+    (err) => res.status(400).send(err)
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
