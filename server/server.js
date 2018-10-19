2432e919e6e8fd64188941e7eb6617a3a4cd6962
Adam Plante <toomanyadams@gmail.com>
2018-10-18 22:57:07 -0700

    Setup Heroku

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { Todo } = require('./models/Todo');
 const { User } = require('./models/User');
 
 const app = express();
-const PORT  = 3000;
+const PORT  = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
