7bd76fc5adcbdf8317017b585d26c7094b5c9ea1
Adam Plante <toomanyadams@gmail.com>
2018-10-19 00:50:43 -0700

    Add patch

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/Todo');
@@ -56,6 +57,28 @@ app.delete('/todos/:id', (req, res) => {
         ? res.status(200).send({ todo })
         : res.status(404).send();
     }).catch(err => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  const body = _.pick(req.body, ['text', 'completed']);
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = Date.now()
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then((todo) => {
+      return todo
+        ? res.status(200).send({ todo })
+        : res.status(404).send();
+    }).catch(err => res.status(500).send());
 });
 
 app.listen(PORT, () => {
