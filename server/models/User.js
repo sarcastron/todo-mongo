ffe9fc79f32c8de20673aa09d4347ba2355b5be6
Adam Plante <toomanyadams@gmail.com>
2018-10-30 00:23:25 -0700

    Improved app config.

--- server/models/User.js
+++ server/models/User.js
@@ -36,7 +36,7 @@ const UserSchema = new mongoose.Schema({
 UserSchema.methods.generateAuthToken = function () {
   const user = this;
   const access = 'auth';
-  const token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+  const token = jwt.sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET).toString();
 
   user.tokens = user.tokens.concat([{ access, token }]);
   return user.save().then(() => {
@@ -56,7 +56,7 @@ UserSchema.statics.findByToken = function (token) {
   let decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (err) {
     return Promise.reject(err);
   }
